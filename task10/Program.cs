155b04bed56a18c1773600edfe9ba2849eb581cf
vanek <ivan2005ab@gmail.com>
2023-08-05 11:57:32 +0300

    task10 done

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int numberOne = number / 10;
+Console.WriteLine(numberOne % 10);
