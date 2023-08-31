c7153bd061dc0c148e5ad500788d47227ec79b64
vanek <ivan2005ab@gmail.com>
2023-08-31 08:23:59 +0300

    task32 done

---
+++ task32/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+
+Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+*/
+
+Console.Write("Укажите длинну массива: ");
+int userNumberLength = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите минимальное значение для заполнения: ");
+int userNumberMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите максимальное значение для заполнения: ");
+int userNumberMax = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray(int userLength, int userMin, int userMax) {
+    int[] array = new int[userLength];
+
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(userMin, userMax + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) {
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int[] Replacement(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int[] userArray = GetRandomArray(userNumberLength, userNumberMin, userNumberMax);
+PrintArray(userArray);
+Replacement(userArray);
+PrintArray(userArray);
