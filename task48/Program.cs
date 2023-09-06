312e32e2d3dd74468a0799238b52816b0ba20d69
vanek <ivan2005ab@gmail.com>
2023-09-06 11:30:27 +0300

    task48 done

---
+++ task48/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+
+Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+*/
+
+Console.Write("Укажите количество рядов: ");
+int userColNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите количество строк: ");
+int userRowNumber = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetRandom2DArray(int colNumber, int rowNumber) {
+    int[,] result = new int[rowNumber,colNumber];
+
+    for (int i = 0; i < rowNumber; i++) {
+        for (int j = 0; j < colNumber; j++) {
+            result[i,j] = i + j;
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] array) {
+    Console.Write($"[№]\t");
+    for (int i = 0; i < array.GetLength(1); i++) {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++) {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] user2DArray = GetRandom2DArray(userRowNumber, userColNumber);
+Print2DArray(user2DArray);
