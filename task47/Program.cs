984019d248b0ad558b30e506051cefe3a352b280
vanek <ivan2005ab@gmail.com>
2023-09-06 11:18:26 +0300

    task47 done

---
+++ task47/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+*/
+
+Console.Write("Укажите количество рядов: ");
+int userColNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите количество строк: ");
+int userRowNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите минимальное значение для заполнения: ");
+int userNumberMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите максимальное значение для заполнения: ");
+int userNumberMax = Convert.ToInt32(Console.ReadLine());
+
+double[,] GetRandom2DArray(int colNumber, int rowNumber, int userMinValue, int userMaxValue) {
+    double[,] result = new double[rowNumber,colNumber];
+
+    for (int i = 0; i < rowNumber; i++) {
+        for (int j = 0; j < colNumber; j++) {
+            result[i,j] = Math.Round(new Random().Next(userMinValue, userMaxValue + 1) + new Random().NextDouble(), 2);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(double[,] array) {
+    Console.Write($"[№]\t");
+    for (int i = 0; i < array.GetLength(1); i++) {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++) {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] user2DArray = GetRandom2DArray(userColNumber, userRowNumber, userNumberMin, userNumberMax);
+Print2DArray(user2DArray);
