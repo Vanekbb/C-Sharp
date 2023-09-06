409f8bbc5ca2f797ea9a4a4f785640fcda922d91
vanek <ivan2005ab@gmail.com>
2023-09-06 13:23:38 +0300

    task51 done

---
+++ task51/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+
+Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д
+
+*/
+
+Console.Write("Укажите количество рядов: ");
+int userColNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите количество строк: ");
+int userRowNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите минимальное значение для заполнения: ");
+int userNumberMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите максимальное значение для заполнения: ");
+int userNumberMax = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetRandom2DArray(int colNumber, int rowNumber, int userMinValue, int userMaxValue) {
+    int[,] result = new int[rowNumber,colNumber];
+
+    for (int i = 0; i < rowNumber; i++) {
+        for (int j = 0; j < colNumber; j++) {
+            result[i,j] = new Random().Next(userMinValue, userMaxValue + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] array) {
+    Console.Write($"[№]\t");
+    for (int i = 0; i < array.GetLength(1); i++) {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++) {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumDiaganalArray(int[,] array) {
+    int result = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        result += array[i,i];
+    }
+    return result;
+}
+
+int[,] user2DArray = GetRandom2DArray(userRowNumber, userColNumber, userNumberMin, userNumberMax);
+Print2DArray(user2DArray);
+Console.WriteLine(SumDiaganalArray(user2DArray));
