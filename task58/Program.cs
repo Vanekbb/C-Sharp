5882980049d09e8f99f8013793653f1b2d54926e
vanek <ivan2005ab@gmail.com>
2023-09-10 11:11:19 +0300

    task58 done

---
+++ task58/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+
+Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+
+Результирующая матрица будет:
+18 20
+15 18
+
+*/
+
+Console.Write("Укажите количество колонок: ");
+int userColNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите количество строк: ");
+int userRowNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите минимальное значение для заполнения: ");
+int userNumberMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите максимальное значение для заполнения: ");
+int userNumberMax = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int userMinValue, int userMaxValue) {
+    int[,] result = new int[rowNumber, colNumber];
+
+    for (int i = 0; i < rowNumber; i++) {
+        for (int j = 0; j < colNumber; j++) {
+            result[i,j] = new Random().Next(userMinValue, userMaxValue + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] array) {
+    Console.Write($"[№]\t");
+    for (int i = 0; i < array.GetLength(1); i++) {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++) {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] TheProductOfMatrix(int[,] firstArray, int[,] secondArray) {
+    int rows = firstArray.GetLength(0);
+    int cols = firstArray.GetLength(1);
+    int[,] result = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i, j] = firstArray[i, j] * secondArray[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] user2DArray = GetRandom2DArray(userRowNumber, userColNumber, userNumberMin, userNumberMax);
+Print2DArray(user2DArray);
+
+Console.WriteLine();
+
+int[,] user2DArray2 = GetRandom2DArray(userRowNumber, userColNumber, userNumberMin, userNumberMax);
+Print2DArray(user2DArray2);
+
+Console.WriteLine();
+
+int[,] user2DArray3 = TheProductOfMatrix(user2DArray, user2DArray2);
+Print2DArray(user2DArray3);
