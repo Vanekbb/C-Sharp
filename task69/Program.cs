d2376ad66b428335d83d2a493621b888b96d864b
vanek <ivan2005ab@gmail.com>
2023-09-13 11:53:42 +0300

    task69 done

---
+++ task69/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+
+Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии
+
+*/
+
+int AToTheDegreeOfB(int a, int b) {
+    if (b == 1) {
+        return a;
+    } else {
+        return a * AToTheDegreeOfB(a, b - 1);
+    }
+}
+
+int result = AToTheDegreeOfB(5, 3);
+
+Console.Write(result);
