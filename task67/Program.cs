c2361094c628ec3da7fe5a4798c74989286d2bba
vanek <ivan2005ab@gmail.com>
2023-09-13 11:33:09 +0300

    task67 done

---
+++ task67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+*/
+
+int FindSumNumbers(int n) {
+    if (n > 0) {
+        return n % 10 + FindSumNumbers(n / 10);
+    } else {
+        return n;
+    }
+}
+
+int result = FindSumNumbers(999);
+Console.WriteLine(result);
